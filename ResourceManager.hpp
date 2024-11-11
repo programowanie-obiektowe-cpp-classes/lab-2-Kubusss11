a1b22ec2303713644f8381d3a152530ce98d953b
Kubusss11 <kuba808x@interia.pl>
2024-11-11 21:48:06 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -2,7 +2,42 @@
 
 #include "Resource.hpp"
 
-class ResourceManager
-{
-    // Twoja implementacja tutaj
+class ResourceManager {
+private:
+    Resource* resource;
+
+public:
+    ResourceManager() : resource(new Resource()) {}
+
+    ~ResourceManager() {
+        delete resource;
+    }
+
+    ResourceManager(const ResourceManager& other) : resource(new Resource(*other.resource)) {}
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            Resource* temp = new Resource(*other.resource);
+            delete resource;
+            resource = temp;
+        }
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& other) noexcept : resource(other.resource) {
+        other.resource = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept {
+        if (this != &other) {
+            delete resource;
+            resource = other.resource;
+            other.resource = nullptr;
+        }
+        return *this;
+    }
+
+    double get() {
+        return resource->get();
+    }
 };
